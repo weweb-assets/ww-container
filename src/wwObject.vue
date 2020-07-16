b571d55d6caffea28525cb009dc45744f52dcf9c
Marc Fabre <nds.kram@gmail.com>
2020-07-16 11:55:48 +0200

    first commit

---
+++ src/wwObject.vue
@@ -0,0 +1,51 @@
+<!-- This is a Vue.js single file component. -->
+<!-- Check the Vue.js doc here :  -->
+<!-- https://vuejs.org/v2/guide/ -->
+
+<!-- This is your HTML -->
+<template>
+    <div class="ww-custom-object">
+        <span class="text">
+            {{ text }}
+        </span>
+    </div>
+</template>
+
+<!-- This is your Javascript -->
+<!-- ✨ Here comes the magic ✨ -->
+<script>
+export default {
+    name: '__COMPONENT_NAME__',
+    props: {
+        // The wwObject controller object is passed. You can access it anytime
+        wwObjectCtrl: Object
+    },
+    data() {
+        return {
+            text: 'I AM WWOBJECT !'
+        };
+    },
+    computed: {
+        // The wwObject contains all the info and data about the wwObject
+        // Use it as you like !
+        wwObject() {
+            return this.wwObjectCtrl.get();
+        }
+    },
+    methods: {},
+    mounted() {
+        this.$emit('ww-loaded', this);
+    }
+};
+</script>
+
+<!-- This is your CSS -->
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add lang="scss" or others to use computed CSS -->
+<style lang="scss" scoped>
+.ww-custom-object {
+    .text {
+        color: red;
+    }
+}
+</style>
