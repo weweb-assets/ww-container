c011fbd377378cb1a2f0a7839bc292bfee8b1a33
Aurélie VIOLETTE <aurelie@weweb.io>
2020-10-18 13:53:53 +0200

    Version 1.0.4

--- src/wwObject.vue
+++ src/wwObject.vue
@@ -1,21 +1,32 @@
 <template>
-    <div class="ww-container">
+    <div class="ww-container" :class="{ selected: isSelected, blue: level % 2 === 0, green: level % 2 === 1 }">
         <wwObject class="ww-container__background" isBackground v-bind="content.background"></wwObject>
         <wwLayout class="ww-container__layout" :style="style" :direction="content.options.direction" path="wwObjects">
-            <template v-slot="{ item }">
-                <wwLayoutItem
-                    class="ww-container__item"
-                    :class="{ editing: isEditing, blue: level % 2 === 0, green: level % 2 === 1 }"
-                >
-                    <wwObject v-bind="item"></wwObject>
+            <template v-slot="{ item, duplicate, index }">
+                <wwLayoutItem class="ww-container__item" :class="{ editing: isEditing }">
+                    <wwObject v-bind="item" noHover></wwObject>
+                    <!-- wwEditor:start -->
+                    <template v-if="isEditing">
+                        <div class="ww-container__resize-handle" :class="content.options.direction"></div>
+                        <button
+                            class="ww-container__duplicate"
+                            :class="content.options.direction"
+                            @click="duplicate(index)"
+                        >
+                            <wwEditorIcon small name="add"></wwEditorIcon>
+                        </button>
+                    </template>
+                    <!-- wwEditor:end -->
                 </wwLayoutItem>
             </template>
         </wwLayout>
-        <!-- wwManager:start -->
-        <div class="ww-container__handle" :class="level % 2 ? 'left' : 'right'" v-if="isEditing">
-            <span class="wwi wwi-align-right"></span>
-        </div>
-        <!-- wwManager:end -->
+        <!-- wwEditor:start -->
+        <template v-if="isEditing">
+            <div class="ww-container__menu" :class="[level % 2 ? 'left' : 'right']">
+                <wwEditorIcon small name="config"></wwEditorIcon>
+            </div>
+        </template>
+        <!-- wwEditor:end -->
     </div>
 </template>
 
@@ -70,6 +81,9 @@ export default {
         isEditing() {
             return this.wwEditorState.editMode === wwLib.wwSectionHelper.EDIT_MODES.CONTENT;
         },
+        isSelected() {
+            return this.wwEditorState.isSelected;
+        },
         level() {
             return this.parentLevel + 1;
         },
@@ -91,6 +105,13 @@ export default {
 .ww-container {
     position: relative;
     box-sizing: border-box;
+    border: 1px solid transparent;
+    &.blue {
+        --ww-container-color: var(--ww-color-blue-500);
+    }
+    &.green {
+        --ww-container-color: var(--ww-color-green-500);
+    }
     &__layout {
         display: flex;
     }
@@ -107,34 +128,77 @@ export default {
         border: 1px solid transparent;
         &.editing:hover {
             border: 1px dashed;
-            &.green {
-                border-color: var(--ww-color-green-500);
-            }
-            &.blue {
-                border-color: var(--ww-color-blue-500);
+            border-color: var(--ww-container-color);
+            > .ww-container__resize-handle,
+            > .ww-container__duplicate {
+                display: flex;
             }
         }
     }
-    &__handle {
+    &:hover,
+    .selected {
+        border: 1px dashed var(--ww-container-color);
+        > .ww-container__menu {
+            display: flex;
+        }
+    }
+    &__menu {
         position: absolute;
-        display: flex;
+        display: none;
         top: 0;
+        border-radius: 100%;
+        padding: var(--ww-spacing-01);
         &.right {
             right: 0;
-            border-radius: 20px 0 0 20px;
+            transform: translate(50%, -50%);
         }
         &.left {
             left: 0;
-            border-radius: 0px 20px 20px 0px;
+            transform: translate(-50%, -50%);
         }
-        background-color: #d02e7c;
+        background-color: var(--ww-container-color);
         z-index: 10;
         color: white;
-        height: 40px;
-        width: 45px;
         justify-content: center;
         align-items: center;
-        font-size: 22px;
+    }
+    &__resize-handle {
+        position: absolute;
+        background-color: var(--ww-container-color);
+        display: none;
+        &.row {
+            height: 24px;
+            width: 6px;
+            top: calc(50% - 12px);
+            right: -3px;
+            cursor: col-resize;
+        }
+        &.column {
+            height: 6px;
+            width: 24px;
+            bottom: -3px;
+            right: calc(50% - 12px);
+            cursor: row-resize;
+        }
+    }
+    &__duplicate {
+        position: absolute;
+        background: var(--ww-container-color);
+        color: white;
+        display: none;
+        padding: var(--ww-spacing-01);
+        border: none;
+        outline: none;
+        border-radius: 100%;
+        &.row {
+            top: 25%;
+            right: 0;
+            transform: translate(50%, -50%);
+        }
+        &.column {
+            right: 25%;
+            transform: translate(-50%, -50%);
+        }
     }
 }
 </style>
