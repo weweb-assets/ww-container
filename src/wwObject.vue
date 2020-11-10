5c70b37d39ca2311e3cc3793511af3d8879807cc
Aurélie VIOLETTE <aurelie@weweb.io>
2020-11-10 18:43:48 +0100

    Add pourcen when lengthInUnit === 100

--- src/wwObject.vue
+++ src/wwObject.vue
@@ -36,7 +36,9 @@
                             @endDrag="endDrag($event)"
                         />
                         <div v-if="isDraging" class="ww-container__units">
-                            {{ content.grid ? content.grid[index] : 0 }}
+                            {{
+                                content.grid ? `content.grid[index]${this.content.lengthInUnit === 100 ? '%' : ''}` : 0
+                            }}
                         </div>
                     </template>
                     <!-- wwEditor:end -->
