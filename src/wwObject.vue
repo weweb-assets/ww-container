dc73f28cfdb2e5a222469c95213f84fdd5dd63c2
Marc Fabre <nds.kram@gmail.com>
2021-03-05 15:37:27 +0100

    Removed states

--- src/wwObject.vue
+++ src/wwObject.vue
@@ -86,9 +86,9 @@ import { getRowConfiguration, getColumnConfiguration } from './configuration';
 
 export default {
     wwDefaultContent: {
-        wwObjects: wwLib.ignoreState([]),
-        grid: wwLib.ignoreState(wwLib.responsive([])),
-        gridDisplay: wwLib.ignoreState(wwLib.responsive([])),
+        wwObjects: [],
+        grid: wwLib.responsive([]),
+        gridDisplay: wwLib.responsive([]),
         direction: wwLib.responsive('row'),
         reverse: wwLib.responsive(false),
         lengthInUnit: wwLib.responsive(100),
